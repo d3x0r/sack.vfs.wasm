78daadc143fb614b062a623f851b554db6e9afdc
d3x0r <d3x0r@no-reply.github.com>
2019-08-18 23:05:25 -0700

    Improve index.js loader script

--- index.js
+++ index.js
@@ -12,16 +12,14 @@ if( isNode ) {
 
 var sackVfsModule;
 try{
-    sackVfsModule = require( "./sack.vfs.wasm.dbg.js" );
-    //console.log( "Got:", sackVfsModule )
+	sackVfsModule = require( "./sack.vfs.wasm.dbg.js" );
 } catch(err1) {
-    try {
-        console.log( err1 );
-        sackVfsModule = require( "./sack.vfs.wasm.js" );
-        //console.log( "Got:", sackVfsModule );
-    } catch(err2) {
-        console.log( err2 );
-    }
+	    try {
+		console.log( "Debug failed; load reloease (normal usually?)", err1 );
+		sackVfsModule = require( "./sack.vfs.wasm.js" );
+	    } catch(err2) {
+		console.log( err2 );
+	    }
 }
 
 if( "undefined" === typeof setImmdediate )
@@ -29,16 +27,16 @@ if( "undefined" === typeof setImmdediate )
 
 
 function Require( path, cb ) {
-	
 	var module;
-        if( typeof path === "string" )
-	        module = require( path );
-        else
-	        module = path;
+	if( typeof path === "string" )
+		module = require( path );
+	else
+		module = path;
 	if( module === sackVfsModule ) {
+		//console.log( "!!!!!!!!! DO NOT RUN AGAIN" );
 		module.onRuntimeInitialized = ()=>{
-			module.FS.chdir( "/home/web_user" );
 			if( isNode ) {
+				//console.log( "mounting NODEFS?" );
 				module.FS.mount( module.NODEFS, {
 						root:".",
 					
@@ -49,22 +47,50 @@ function Require( path, cb ) {
 				module.FS.mount( module.IDBFS, { }
 					, "/home/web_user" );
 				module.FS.syncfs( true /*load*/, (err)=>{
-					console.log( "Reaload complete:", err );
+					if( err ) {
+						
+						console.log( "Reaload complete:", err );
+					}
+					else
+						doInit();
 				} )
 			}
-			try {
-				module._initFS();
-			} catch(err) {
-				console.log( "Init has failed", err );
+			
+			// this really needs some sort of indicator that activity happened.
+			// disk activity light?
+			function doSync() {
+				module.FS.syncfs( false /*load*/, (err)=>{
+					if( err ) {
+						console.log( "Sync Error:", err );
+					}
+					else {
+						//console.log( "Sync complete; no error" );
+						setTimeout( doSync, 5000 );
+					}
+				} )
+				
 			}
-			const sack = module.SACK;
-			if( typeof window !== "undefined" )
-				window.SACK = sack;
-			setImmediate( ()=>cb( sack ) );
+			
+			
+			function doInit() {
+				try {
+					module._initFS();
+				} catch(err) {
+					console.log( "Init has failed", err );
+					return;
+				}
+				module.FS.chdir( "/home/web_user" );
+				setTimeout( doSync, 5000 );
+			
+				const sack = module.SACK;
+				if( typeof window !== "undefined" )
+					window.SACK = sack;
+				cb( sack );
+			}
 		}
 	}else{
 		console.log( "Other Require (???)", path, cb );
-		setImmediate( ()=>cb( module ) );
+		cb( module );
 	}
 }
 
@@ -74,7 +100,6 @@ function Loader(cb) {
 
 
 if( typeof module !== "undefined" )  {
-	module.exports=exports=Loader;
-	
+	module.exports=exports=new Promise( Loader );
 }
 
